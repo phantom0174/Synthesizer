30f30feb646c76317b56d6c25133e6f899919429
Phantom0174 <darrenhsiou@gmail.com>
2020-12-14 09:34:16 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ async def Auto_task():
     AdminRole = guild.get_role(db['Admin'])
     while True:
         if 21 <= now_time_info('hour') <= 23 or 0 <= now_time_info('hour') <= 6:
-            data.execute('SELECT Id, Status FROM account')
+            data.execute('SELECT Id, Status FROM account;')
             Accs = data.fetchall()
 
             for acc in Accs:
@@ -45,6 +45,7 @@ async def Auto_task():
         if dyn['ldbh'] != now_time_info('hour'):
             file_name = 'db_backup/' + str(now_time_info('hour')) + '_backup.db'
             bck_db_conn = sqlite3.connect(file_name)
+            await asyncio.sleep(20)
             sqlitebck.copy(connection, bck_db_conn)
 
             dyn['ldbh'] = now_time_info('hour')
