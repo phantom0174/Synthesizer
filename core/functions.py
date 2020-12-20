cee3a44645093c01e49b2ed20972a962bd639d09
phantom0174 <darrenhsiou@gmail.com>
2020-12-21 00:36:37 +0800

    bug fix

--- core/functions.py
+++ core/functions.py
@@ -8,46 +8,46 @@ def now_time_info(mode):
 
     if mode == 'whole':
         return str(dt2.strftime("%Y-%m-%d %H:%M:%S"))
-    elif mode == 'hour':
+    if mode == 'hour':
         return int(dt2.strftime("%H"))
-    elif mode == 'date':
+    if mode == 'date':
         return int(dt2.isoweekday())
 
 
 def cadre_trans(cadre):
     if cadre == '副召':
         return 1
-    elif cadre == 1:
+    if cadre == 1:
         return '副召'
 
     if cadre == '網管':
         return 2
-    elif cadre == 2:
+    if cadre == 2:
         return '網管'
 
     if cadre == '議程':
         return 3
-    elif cadre == 3:
+    if cadre == 3:
         return '議程'
 
     if cadre == '公關':
         return 4
-    elif cadre == 4:
+    if cadre == 4:
         return '公關'
 
     if cadre == '美宣':
         return 5
-    elif cadre == 5:
+    if cadre == 5:
         return '美宣'
 
     if cadre == '管理':
         return 6
-    elif cadre == 6:
+    if cadre == 6:
         return '管理'
 
     if cadre == '一般':
         return 7
-    elif cadre == 7:
+    if cadre == 7:
         return '一般'
 
     return -1
--- core/setup.py
+++ core/setup.py
@@ -1,10 +1,13 @@
-import json
-import pymongo
 from pymongo import MongoClient
+import pymongo
+import json
+import os
 
 # database setup
-link = "mongodb+srv://bot-console:XgZLiNW33wrY6tqi@light-cube-cluster.5wswq.mongodb.net/syn?retryWrites=true&w=majority"
-client = pymongo.MongoClient(link).Synthesizer
+password = os.environ.get("PW")
+account = os.environ.get("ACCOUNT")
+link = f"mongodb+srv://{account}:{password}@light-cube-cluster.5wswq.mongodb.net/syn?retryWrites=true&w=majority"
+client = MongoClient(link).Synthesizer
 
 with open('setting.json', mode='r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
