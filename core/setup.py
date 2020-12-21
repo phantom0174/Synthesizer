271b051bb2d77b80e8222b8606450009b1d790f7
phantom0174 <darrenhsiou@gmail.com>
2020-12-21 18:44:04 +0800

    add requirements.txt

--- core/setup.py
+++ core/setup.py
@@ -7,7 +7,7 @@ import os
 password = os.environ.get("PW")
 account = os.environ.get("ACCOUNT")
 link = f"mongodb+srv://{account}:{password}@light-cube-cluster.5wswq.mongodb.net/syn?retryWrites=true&w=majority"
-client = MongoClient(link).Synthesizer
+client = MongoClient(link).synthesizer
 
 with open('setting.json', mode='r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
